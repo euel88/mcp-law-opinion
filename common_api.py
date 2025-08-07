efb0250e5a429e742c0e625523dc46272a86ff4a
euel88 <poneter@naver.com>
2025-08-07 16:06:25 +0900

    Update common_api.py

--- common_api.py
+++ common_api.py
@@ -571,7 +571,7 @@ class OpenAIHelper:
                     {"role": "user", "content": user_prompt}
                 ],
                 temperature=0.3,
-                max_tokens=2000
+                max_completion_tokens=2000
             )
             
             return response.choices[0].message.content
